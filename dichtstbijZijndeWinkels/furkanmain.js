ba6dee70a9cc9b7509a6dc6c69803b3a89b213a6
furkanzelik <furkanzelik@gmail.com>
2023-03-18 18:19:42 +0300

    18.03.2023

---
+++ dichtstbijZijndeWinkels/furkanmain.js
@@ -0,0 +1,16 @@
+
+
+let city = [{
+    state: ` Rotterdam `
+},{
+    state: ` Amsterdam `
+},{
+    state: ` Gouda `
+},{
+    state: ` Schoonhoven `
+},{
+    state: ` Den Haag `
+},{
+    state: ` Utrecht `
+}, ];
+
